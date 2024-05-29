84156278661c707a91bd0f49650da4127ebdca98
Lilla Kavecsanszki <lilla@Lilla-MacBook-Air.local>
2024-05-29 11:55:35 +0200

    c# basics added

--- ConsoleApp1/MyFirstProject.cs
+++ ConsoleApp1/MyFirstProject.cs
@@ -133,30 +133,162 @@ using System.Threading.Tasks;
 // }
 
 
-namespace Operators
+// namespace Operators
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+            // int age = 23;
+            
+            // age++;
+            // // age = age + 1;
+            // // age += 1;
+            // Console.WriteLine(age);
+
+            // age--;
+            // Console.WriteLine(age);
+
+            // age += 10;
+            // Console.WriteLine(age);
+
+            // age -= 10;
+            // Console.WriteLine(age);
+
+            // age *= 2;
+            // Console.WriteLine(age);
+
+//             double age = 23;
+//             // + - * /
+//             age /= 10; //2.3 -> 2
+//             Console.WriteLine(age);
+
+//             string name = "Lilla";
+//             name += " is programming";
+
+//             char ch = 'a';
+//             ch += 'b';
+//             Console.WriteLine(ch);
+
+//             Console.WriteLine(name);
+
+//             int i = 0;
+//             // i++;
+//             // Console.WriteLine(i);
+
+//             Console.WriteLine(i++); //0
+//             Console.WriteLine(i);
+
+//         }
+//     }
+// }
+
+
+// Remainder
+
+// namespace Modulus
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var firstNum = 10;
+//             int secondNum = 3;
+
+//             Console.WriteLine(firstNum / secondNum); //3
+
+//             Console.WriteLine(firstNum % secondNum); //1 - the remainder as 3*3 = 9, 10-9 = 1
+//         }
+//     }
+// } 
+
+
+// Constants - VAR (variable but only if you sure in the data type) CONST cannot be changed
+// namespace Constants 
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             const int vat = 20;
+//             const double percentVAT = vat / 100D;
+
+//             int balance = 1000;
+
+//             Console.WriteLine(balance * percentVAT); 
+
+//             Console.WriteLine(vat); 
+//         }
+//     }
+// } 
+
+
+// odd even checker exercise
+// namespace Constants 
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int num1 = 10;   //declaring a variable with int etc...
+//             int num2 = 2;
+//             int remainder = num1 % num2;
+//             System.Console.WriteLine(remainder);
+
+//             num1 = 11;       //no need for the int, as now we are reassigning the variable
+//             remainder = num1 % num2;
+//             System.Console.WriteLine(remainder);
+            
+//         }
+//     }
+// } 
+
+
+//Console input/Output
+namespace ConsoleIO 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int age = 23;
-            
-            age++;
-            // age = age + 1;
-            // age += 1;
-            Console.WriteLine(age);
+            Console.WriteLine("Hello my name is Lilla!");
+            Console.Write("Enter you name: ");
 
-            age--;
-            Console.WriteLine(age);
+            string name;
+            name = Console.ReadLine(); // can be 1 line too; string name = Console.Readline();
+            // Console.WriteLine(name);
 
-            age += 10;
-            Console.WriteLine(age);
+            Console.Write("Enter your age: ");
+            string ageInput = Console.ReadLine(); // these can be in 1 line too; int age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(ageInput);
+            // Console.WriteLine(age);
 
-            age -= 10;
-            Console.WriteLine(age);
+            // Console.Write("Your name is ");
+            // Console.Write(name);
+            // Console.Write("and your age is ");
+            // Console.Write(age + ".");
 
-            age *= 2;
-            Console.WriteLine(age);
+            Console.Write("Your name is " + name + " and your age is " + age + ".");
+            System.Console.WriteLine();
+
+            // == > >= < <= != || &&
+
+            if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Invalid age!");
+            }
+            else 
+            {
+                if (age >= 18 && age <= 25)
+                {
+                    Console.WriteLine("You are between 18 and 25!");
+                }
+                else if (age >= 26)
+                {
+                    Console.WriteLine("You are 26 or older!");
+                }
+            }
+        
         }
     }
-}
+} 
